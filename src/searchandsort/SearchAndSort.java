3b3044b5ea2f9e429aad62c4365a624c20d865f3
bj.brassard <bj.brassard@10.0.1.39>
2020-03-29 10:17:55 -0600

    Implemented the mergeSort method but haven't seemed to get it to work yet

--- src/searchandsort/SearchAndSort.java
+++ src/searchandsort/SearchAndSort.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class SearchAndSort {
 
     private static Random rng = new Random();
-    private static final int SIZE_THRESHOLD = 11;
+    private static final int SIZE_THRESHOLD = 20;
     // These statements create a random list of integers.
 
     public static List<Integer> createList(int size) {
@@ -83,7 +83,7 @@ public class SearchAndSort {
     * @param Using List<Integer> newList (List)
     * @result A list that has been search through with binary search
     *
-    */
+     */
     public static int binarySearch(List<Integer> newList, int targetVal) {
         // Returns -1 if there is no match
         int result = -1;
@@ -100,24 +100,22 @@ public class SearchAndSort {
         while (low < high && result < 0) {
             // Set the middle value
             int mid = (low + high) / 2;
-            
+
             // If the low is the target value, then result equals the low
             if (targetVal == newList.get(low)) {
                 result = low;
             } // if 
-            
             // If the mid is the target value, then result equals the mid
             else if (targetVal == newList.get(mid)) {
                 result = mid;
             } // else if
-            
             // If the high is the target value, then result equals the high
             else if (targetVal == newList.get(high)) {
                 result = high;
             } // else if
             // If targetVal is less the mid, high equals the mid value -1
-            else if (targetVal < newList.get(mid)){
-                high = mid -1;
+            else if (targetVal < newList.get(mid)) {
+                high = mid - 1;
             } // else if
             // If targetVal is more the mid, low equals the mid value +1
             else {
@@ -126,55 +124,54 @@ public class SearchAndSort {
         } // while loop
         return result;
     } // binarySearch(List<Integer> newList)
-    
+
     // Makes a swap method to make the sort methods more clean
     public static void swap(List<Integer> newList, int i, int j) {
         // Swaps two integers
         int tempNum = newList.get(i);
         newList.set(i, newList.get(j));
         newList.set(j, tempNum);
-        
+
     } // swap(List<Integer> newList
-    
+
     // Find the minimum value in a list and makes code for sorts more clean
-    public static int findMinPoint(List<Integer> newList, int start){
+    public static int findMinPoint(List<Integer> newList, int start) {
         // Creating variable that changes to different min points
         int currentPoint = start;
         // Finding lowest possible integer then setting it to the start
-        for (int i = start + 1; i < newList.size(); i++){
-            if (newList.get(i) < newList.get(currentPoint)){
+        for (int i = start + 1; i < newList.size(); i++) {
+            if (newList.get(i) < newList.get(currentPoint)) {
                 currentPoint = i;
             } // if
         } // for loop
         return currentPoint;
     } // findMinPoint(List<Integer> newList
-        
+
     /* Find a method that sorts a list using selection
     *  sort. Should put all integers in the list in order
     *  from smallest to largest.
     *
     *  @param newList Sorted list (List)
     *  @return newList ending list, should be sorted
-    */
-    
-    public static void selectionSort(List<Integer> newList){
-       for (int i = 0; i < newList.size(); i++){
-           int j = findMinPoint(newList, i);
-           swap(newList, i, j);
-       } // for loop
+     */
+    public static void selectionSort(List<Integer> newList) {
+        for (int i = 0; i < newList.size(); i++) {
+            int j = findMinPoint(newList, i);
+            swap(newList, i, j);
+        } // for loop
     } // selectionSort(List<Integer> newList)
-    
+
     // Helps compress the insertionSort method
-    public static void insert(List<Integer> newList, int nextNum){
+    public static void insert(List<Integer> newList, int nextNum) {
         int i = nextNum;
-        while (i > 0 && newList.get(i) < newList.get(i - 1)){
+        while (i > 0 && newList.get(i) < newList.get(i - 1)) {
             swap(newList, i, i - 1);
             i = i - 1;
-            
+
         } // while loop
-        
+
     } // insert(List<Integer> newList)
-    
+
     /* Find a method that sorts a list of integers that uses the
      * insertion sort method
      *
@@ -182,16 +179,74 @@ public class SearchAndSort {
      * @param next
      * @return swap values in list and sorts it
      */
-    
-    public static void insertionSort(List<Integer> newList){
-        for (int i = 1; i < newList.size(); i++){
+    public static void insertionSort(List<Integer> newList) {
+        for (int i = 1; i < newList.size(); i++) {
             insert(newList, i);
-            
+
         } // for loop
     } // insertionSort(List<Integer>)
-   
-            
-            
+
+    /* Find a method that sorts a list of integers
+    *  using the merge sort method
+    *
+    *  @param NewList
+    *  @param prefixStart, suffixStart, suffixEnd
+    *  @result Sorted newList
+     */
+    // Borrowed from Leon Tabak
+    public static void merge(List<Integer> newList, int prefixStart,
+            int suffixStart, int suffixEnd) {
+        List<Integer> temp = new ArrayList<>();
+
+        int i = prefixStart;
+        int j = suffixStart;
+
+        while (i < suffixStart && j < suffixEnd) {
+            if (newList.get(i) < newList.get(j)) {
+                temp.add(newList.get(i));
+                i++;
+            } // if
+            else {
+                temp.add(newList.get(j));
+                j++;
+            } // else
+        } // while
+
+        while (i < suffixStart) {
+            temp.add(newList.get(i));
+            i++;
+        } // while
+
+        while (j < suffixEnd) {
+            temp.add(newList.get(j));
+            j++;
+        } // while
+
+        i = prefixStart;
+        for (int index = 0; index < temp.size(); index++) {
+            newList.set(i, temp.get(index));
+            i++;
+        } // for
+    } // merge( List<Integer>, int, int )        
+
+    // Borrowed from Leon Tabak    
+    public static void mergeSort(List<Integer> newList) {
+        for (int stepSize = 2; stepSize < newList.size(); stepSize *= 2) {
+            for (int i = 0; i < newList.size(); i += stepSize) {
+                int prefixStart = i;
+                int suffixStart = i + stepSize / 2;
+                int suffixEnd = Math.min(newList.size(), i + stepSize);
+                merge(newList, prefixStart, suffixStart, suffixEnd);
+            } // for
+            if (stepSize > newList.size() / 2) {
+                int prefixStart = 0;
+                int suffixStart = stepSize;
+                int suffixEnd = newList.size();
+                merge(newList, prefixStart, suffixStart, suffixEnd);
+            } // if
+        } // for
+    } // mergeSort(List<Integer> newList)  
+
     public static void main(String[] args) {
         System.out.println("Selection Sort");
         List<Integer> data = createList(10);
@@ -199,18 +254,33 @@ public class SearchAndSort {
         System.out.println(" **** ");
         selectionSort(data);
         printList(data);
-        
+
         System.out.println(" **** ");
-        
+
         System.out.println("Insertion Sort");
         data = createList(10);
         printList(data);
         System.out.println(" **** ");
         insertionSort(data);
         printList(data);
-        
+
+        System.out.println(" **** ");
+
+        System.out.println("Merge Sort");
+        data = createList(10);
+        printList(data);
+        System.out.println(" **** ");
+        mergeSort(data);
+        printList(data);
 
     } // main( String [] )
 
 } // SearchAndSort
 
+/* These examples of how to make the different methods and classes are
+*  examples of how java can seem more complex than Python, which a lot of you
+*  only have experience in. One of the easiest difference that are seen are
+*  that in Python, one can code the same thing in Java but with much less lines.
+*  Java is one of the foundation coding languages to leanr and once one learns it,
+*  most of the other coding languages will become more clear
+*/
